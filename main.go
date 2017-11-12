e2cb34cde4c3ef2cfbef9c6090e706658ed2b402
RedAngel121 <redangel121@gmail.com>
2017-11-11 23:11:18 -0500

    simple hello world thing that shows up in port 8080

--- main.go
+++ main.go
@@ -13,10 +13,10 @@ http.HandleFunc("/", index) //if slash then set index
 	http.ListenAndServe(":8080", nil) // the address used to show the response
 }
 
-//
+// This is the index from above where you need to make a response and display it
 
-func index(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "whatever")
-	return
+func index(writer http.ResponseWriter, request *http.Request) { //the index fall back pattern that uses a HTTP header for easy browser access
+	fmt.Fprintf(writer, "whatever") // The Hello World line displayed on the port in the browser
+	return // end of the code
 }
 // COMMENT
