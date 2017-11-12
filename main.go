d9da11ca50be9d0444cfef4933bed367274816ca
RedAngel121 <redangel121@gmail.com>
2017-11-12 12:32:14 -0500

    simple hello world thing that shows up in port 8080

--- main.go
+++ main.go
@@ -1,22 +1,16 @@
-// This is the file name and main starting point for then program
 package main
-// Importing the stuff that the language needs to run itself
+
 import (
-	"net/http" //web import
-	"fmt" //formatting import
+	"net/http"
+	"fmt"
 )
 
-// Main function that handles the arguments and address info for the response
-
 func main() {
-http.HandleFunc("/", index) //if slash then set index
-	http.ListenAndServe(":8080", nil) // the address used to show the response
+http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
-// This is the index from above where you need to make a response and display it
-
-func index(writer http.ResponseWriter, request *http.Request) { //the index fall back pattern that uses a HTTP header for easy browser access
-	fmt.Fprintf(writer, "whatever") // The Hello World line displayed on the port in the browser
-	return // end of the code
+func index(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "whatever")
+	return
 }
-// COMMENT
