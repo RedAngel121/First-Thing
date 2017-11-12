3fba0879b1d4c5f919ceb372eb847172553f5eec
RedAngel121 <redangel121@gmail.com>
2017-11-12 12:34:46 -0500

    simple hello world thing that shows up in port 8080

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 )
 
+// Function initialization, port 8080 listen and serve for index
+
 func main() {
 http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
 }
+
+// index response and HTTP header and body creation for assigned formatting
 
 func index(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "whatever")
